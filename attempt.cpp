0d141fe90079f0d720846b0148806ac41854e4c1
sakshamsaxena <ontenth@live.com>
2016-02-28 13:16:21 +0530

    Added annonations

--- attempt.cpp
+++ attempt.cpp
@@ -5,20 +5,27 @@ using namespace cv;
 int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
+
     if(!cap.isOpened())  // check if we succeeded
         return -1;
-    int wd = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-    int ht = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-    int oht = floor(ht * 0.7);
-    Mat edges;
-    Mat cl;
-    Rect r(0, ht - oht, wd, oht);
-    // namedWindow("edges",1);
+
+    // Constants
+    int fWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //True frame width
+    int fHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //True frame height
+    int cHeight = floor(fHeight * 0.7); // Cropped height
+    int cWidth = fWidth; // Width remains same
+    int X = 0; // X of Top left corner of Rectangle
+    int Y = fHeight - cHeight; // Y of Top left corner of Rectangle
+    
+    Mat frame;
+    Mat croppedFrame;
+    Rect cropper(X, Y, cWidth, cHeight);
+    
     for(;;)
     {
-        cap >> edges;
-        cl = edges(r);
-        imshow("cropped", cl);
+        cap >> frame;
+        croppedFrame = frame(cropper);
+        imshow("cropped", croppedFrame);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
