7491b07a3f1a5138a8fb8bf5b098557558de91b5
sakshamsaxena <ontenth@live.com>
2016-02-28 13:41:43 +0530

    Frame rate limited to 10. More annonations

--- attempt.cpp
+++ attempt.cpp
@@ -9,6 +9,9 @@ int main(int, char**)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
+    // Set FPS to 10
+    cap.set(CV_CAP_PROP_FPS, 10);
+
     // Constants
     int fWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //True frame width
     int fHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //True frame height
@@ -17,15 +20,17 @@ int main(int, char**)
     int X = 0; // X of Top left corner of Rectangle
     int Y = fHeight - cHeight; // Y of Top left corner of Rectangle
     
+    // Frame matrix and cropping rectangle initialization
     Mat frame;
     Rect cropper(X, Y, cWidth, cHeight);
     
+    // Iterating over each frame
     for(;;)
     {
         cap >> frame; // Webcam image to an empty frame
         frame = frame(cropper); // Cropping same frame
         cvtColor(frame, frame, CV_BGR2GRAY); // Grayscale conversion of same frame
-        threshold(frame, frame, 200, 255, THRESH_TOZERO);
+        threshold(frame, frame, 200, 255, THRESH_TOZERO); // Threshold at 200 value
         imshow("cropped", frame);
         if(waitKey(30) >= 0) break;
     }
