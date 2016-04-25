bf04adafcbd377bb0b3c09c15eb128ac2646d080
sakshamsaxena <ontenth@live.com>
2016-04-26 01:29:18 +0530

    All together now

--- attempt.cpp
+++ attempt.cpp
@@ -6,13 +6,13 @@ using namespace cv;
 
 int main(int, char**)
 {
-    VideoCapture cap("./Trip_LowRes.mp4"); // open the default camera
+    VideoCapture cap(0); // open the default camera
 
     wiringPiSetup () ;
     pinMode (0, OUTPUT) ;
 
     // Set FPS to 10
-    cap.set(CV_CAP_PROP_FPS, 10);
+    // cap.set(CV_CAP_PROP_FPS, 10);
 
     // Constants
     int fWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //True frame width
@@ -21,17 +21,17 @@ int main(int, char**)
     int cWidth = fWidth; // Width remains same
     int X = 0; // X of Top left corner of Rectangle
     int Y = fHeight - cHeight; // Y of Top left corner of Rectangle
-    
+
     // Frame matrix and cropping rectangle initialization
     Mat frame;
     Rect cropper(X, Y, cWidth, cHeight);
-    
-    
+    int _delta = 5;
+    int _min_area = 800;
+    int _max_area = 3000;
     std::vector<std::vector<Point> > contours;
     std::vector<Rect> bboxes;
-    Ptr< MSER > mser = MSER::create();
+    Ptr< MSER > mser = MSER::create(_delta, _min_area, _max_area);
 
-    
     // Iterating over each frame
     for(;;)
     {
@@ -41,10 +41,11 @@ int main(int, char**)
         threshold(frame, frame, 200, 255, THRESH_TOZERO); // Threshold at 200 value
         mser->detectRegions(frame, contours, bboxes);
 
-	if(bboxes.size())
-		digitalWrite (0,  LOW) ;
+	if(bboxes.size()){
+		digitalWrite (0,  HIGH) ;
+	}
 	else
-		digitalWrite (0,  HIGH) 
+		digitalWrite (0,  LOW);
 
         imshow("cropped", frame);
         if(waitKey(30) >= 0) break;
